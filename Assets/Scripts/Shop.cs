5c1a8998e0f1f44d20a1b6202bbc52bb53da4182
skv1rtyyy <denysja1@gmail.com>
2025-07-30 21:12:12 +0300

    Shop1

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private int itemPrice;
+    [SerializeField] private string itemID;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private Text priceText;
+
+    private const string CoinsKey = "coins";
+    private string PurchasedKey => $"shop_item_{itemID}_purchased";
+
+    void Start()
+    {
+        priceText.text = itemPrice + " $";
+        buyButton.onClick.AddListener(BuyItem);
+
+        if (IsPurchased())
+        {
+            MarkAsPurchased();
+        }
+    }
+
+    private void BuyItem()
+    {
+        int currentCoins = PlayerPrefs.GetInt(CoinsKey, 0);
+
+        if (currentCoins >= itemPrice && !IsPurchased())
+        {
+            currentCoins -= itemPrice;
+            PlayerPrefs.SetInt(CoinsKey, currentCoins);
+            PlayerPrefs.SetInt(PurchasedKey, 1);
+            PlayerPrefs.Save();
+
+            MarkAsPurchased();
+            Debug.Log($"{itemID} куплено за {itemPrice} монет.");
+        }
+        else
+        {
+            Debug.Log("Недостатньо монет або вже куплено.");
+        }
+    }
+
+    private bool IsPurchased()
+    {
+        return PlayerPrefs.GetInt(PurchasedKey, 0) == 1;
+    }
+
+    private void MarkAsPurchased()
+    {
+        buyButton.interactable = false;
+        priceText.text = "Purchased";
+    }
+}

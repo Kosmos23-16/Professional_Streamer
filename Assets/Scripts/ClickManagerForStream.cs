666ebadd308ea90235d0e1d8028fd3f093255aae
skv1rtyyy <denysja1@gmail.com>
2025-08-12 18:21:24 +0300

    1

--- Assets/Scripts/ClickManagerForStream.cs
+++ Assets/Scripts/ClickManagerForStream.cs
@@ -51,6 +51,8 @@ public class ClickManagerForStream : MonoBehaviour
 
         likes += likeBonus;
         likesToNextFollower += likeBonus;
+        AchievementsManager.Instance?.CheckAchievements(coins);
+
 
         if (likesToNextFollower >= followerLikeThreshold)
         {
@@ -146,4 +148,10 @@ public class ClickManagerForStream : MonoBehaviour
         rewardFigure2?.SetActive(false);
         rewardFigure3?.SetActive(false);
     }
+    public void AddCoins(int amount)
+    {
+        coins += amount;
+        SaveData();
+    }
+
 }

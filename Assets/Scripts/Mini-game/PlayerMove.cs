94674654aaa11447c54affb9ff1d422b7d813c1c
Kosmos23-16 <kosmosmcdevinos@gmail.com>
2025-08-14 02:42:26 +0300

    test

--- Assets/Scripts/Mini-game/PlayerMove.cs
+++ Assets/Scripts/Mini-game/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -5,11 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+
+    private string idleTrigger = "idle";
+    private string walkTrigger = "walk";
+
     private CharacterController controller;
+   private Animator animator;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,5 +27,16 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
 
         controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (move.magnitude > 0.1f)
+        {
+            animator.ResetTrigger(idleTrigger);
+            animator.SetTrigger(walkTrigger);
+        }
+        else
+        {
+            animator.ResetTrigger(walkTrigger);
+            animator.SetTrigger(idleTrigger);
+        }
     }
 }

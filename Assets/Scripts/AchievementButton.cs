0a367def1cedcec7f18d2460852ee24368eb8c72
skv1rtyyy <denysja1@gmail.com>
2025-08-28 19:47:48 +0300

    rewardClaimed

--- Assets/Scripts/AchievementButton.cs
+++ Assets/Scripts/AchievementButton.cs
@@ -4,12 +4,32 @@ using UnityEngine.UI;
 public class AchievementButton : MonoBehaviour
 {
     public string achievementId;
+    public Text rewardMessageText;
+    public string customMessage;
     private Button button;
+
+    private string messageKey;
 
     void Start()
     {
         button = GetComponent<Button>();
+        messageKey = $"{achievementId}_message";
+
         UpdateInteractable();
+
+        if (rewardMessageText != null)
+        {
+            string savedMessage = PlayerPrefs.GetString(messageKey, "");
+            if (!string.IsNullOrEmpty(savedMessage))
+            {
+                rewardMessageText.text = savedMessage;
+                rewardMessageText.gameObject.SetActive(true);
+            }
+            else
+            {
+                rewardMessageText.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void ClaimReward()
@@ -21,7 +41,20 @@ public class AchievementButton : MonoBehaviour
             FindObjectOfType<ClickManagerForStream>()?.RefreshCoins();
             FindObjectOfType<CoinsDisplay>()?.RefreshCoinsUI();
 
+            ShowRewardMessage();
             UpdateInteractable();
+        }
+    }
+
+    private void ShowRewardMessage()
+    {
+        if (rewardMessageText != null)
+        {
+            rewardMessageText.text = customMessage;
+            rewardMessageText.gameObject.SetActive(true);
+
+            PlayerPrefs.SetString(messageKey, customMessage);
+            PlayerPrefs.Save();
         }
     }
 
